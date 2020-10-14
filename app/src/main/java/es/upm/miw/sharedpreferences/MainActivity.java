05a8f6107a5c46e2c13ee389a801c8f2c9881ebd
btabuenca <bernardo.tabuenca@gmail.com>
2020-10-15 00:41:05 +0200

    First version

---
+++ app/src/main/java/es/upm/miw/sharedpreferences/MainActivity.java
@@ -0,0 +1,69 @@
+package es.upm.miw.sharedpreferences;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView textView;
+    private EditText editText;
+    private Button button;
+
+    private static final String SHARED_PREF_NAME = "username";
+    private static final String KEY_NAME = "key_username";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+        editText = findViewById(R.id.editText);
+        button = findViewById(R.id.button);
+
+        displayName();
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveName();
+            }
+        });
+    }
+
+    private void displayName() {
+        SharedPreferences sp = getSharedPreferences(SHARED_PREF_NAME, MODE_PRIVATE);
+        String name = sp.getString(KEY_NAME, null);
+
+        if (name != null) {
+            textView.setText("Welcome " + name);
+        }
+    }
+
+    private void saveName() {
+        String name = editText.getText().toString();
+
+        // Check if editText is empty
+        if (name.isEmpty()) {
+            editText.setError("Oops! No Name");
+            editText.requestFocus();
+            return;
+        }
+
+        SharedPreferences sp = getSharedPreferences(SHARED_PREF_NAME, MODE_PRIVATE);
+        SharedPreferences.Editor editor = sp.edit();
+
+        editor.putString(KEY_NAME, name);
+
+        editor.apply();
+
+        // Empty the editText
+        editText.setText("");
+
+        // Optional, to display the name after shared prefs saved.
+        displayName();
+    }
+}
