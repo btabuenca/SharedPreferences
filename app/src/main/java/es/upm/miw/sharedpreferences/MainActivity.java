44b9c3c35042bb7241b2c4d1e5494dfc47651c3b
btabuenca <bernardo.tabuenca@gmail.com>
2021-10-17 19:55:46 +0200

    Vesion 2021

--- app/src/main/java/es/upm/miw/sharedpreferences/MainActivity.java
+++ app/src/main/java/es/upm/miw/sharedpreferences/MainActivity.java
@@ -1,11 +1,12 @@
 package es.upm.miw.sharedpreferences;
 import android.content.SharedPreferences;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     private TextView textView;
